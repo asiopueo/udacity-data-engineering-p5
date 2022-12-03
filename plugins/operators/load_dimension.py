0a6ea26890bf9aeee2cca8069aa5641776adb62c
Martin Lippl <martin@lippl.tech>
2022-12-03 15:02:05 +0100

    Extended load_dimension operator

--- plugins/operators/load_dimension.py
+++ plugins/operators/load_dimension.py
@@ -10,14 +10,21 @@ class LoadDimensionOperator(BaseOperator):
     def __init__(self,
                  redshift_conn_id,
                  sql_query,
+                 table,
+                 append_mode,
                  *args, **kwargs):
 
         super(LoadDimensionOperator, self).__init__(*args, **kwargs)
-        self.redshift_conn_id = redshift_conn_id,
+        self.redshift_conn_id = redshift_conn_id
         self.sql_query = sql_query
-        
+        self.table = table
+        self.append_mode = append_mode
 
     def execute(self, context):
+        self.log.info(f'Starting load of dimension table {self.table}')
         redshift = PostgresHook( self.redshift_conn_id )
-        self.log.info('Loading dimension table...')
-        redshift.run( self.sql_query )
+        if self.append_mode == True:
+            redshift.run( self.sql_query )
+        else:
+            redshift.run( self.sql_query )
+        self.log.info(f'Finished load of table {self.table}')
